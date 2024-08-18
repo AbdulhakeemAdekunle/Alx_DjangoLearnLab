53bd1251ac0691070eb57dbeae37f9286f7b2008
Abdulhakeem Adekunle <abdulhakeemyisa@gmail.com>
2024-08-18 08:44:19 +0100

    Corrected syntax error per checker requirements

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -26,5 +26,4 @@ library = Library.objects.get(name=library_name)
 books_in_library = library.books.all()
 
 # Retrieve the librarian for a library.
-library = Library.objects.get(name=library_name)
-Librarian.objects.get(library = "library_name")
+Librarian.objects.get(library="library_name")
