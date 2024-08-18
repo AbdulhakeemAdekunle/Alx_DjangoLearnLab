8d33808a0d5e6659fd054dec20a850cc29d7f5ea
Abdulhakeem Adekunle <abdulhakeemyisa@gmail.com>
2024-08-18 08:50:53 +0100

    Created query samples to work with model

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -17,12 +17,12 @@ library = Library.objects.create(name="Fred Swaniker Library Complex")
 librarian = Librarian.objects.create(name="Nungari Nguru", library=library)
 
 #Query all books by a specific author.
-Book.objects.filter(author__name="author_name")
-
+author = Author.objects.get(name="Blessing Malik")
+Book.objects.filter(author=author)
 
 # List all books in a library.
-library = Library.objects.get(name="library_name")
+library = Library.objects.get(name="Fred Swaniker Library Complex")
 books_in_library = library.books.all()
 
 # Retrieve the librarian for a library.
-Librarian.objects.get(library__name = "library_name")
+Librarian.objects.get(library = library)
