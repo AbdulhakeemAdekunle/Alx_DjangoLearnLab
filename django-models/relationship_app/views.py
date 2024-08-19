64f67573632a3d6a68e8304174c353d24dbb040b
Abdulhakeem Adekunle <abdulhakeemyisa@gmail.com>
2024-08-19 11:19:33 +0100

    created a function based view that listed all books in the library

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from . models import Book, Author, Library, Librarian
 
 # Create your views here.
+
+"""
+Implement Function-based View:
+
+Create a function-based view in relationship_app/views.py that lists all books stored in the database.
+This view should render a simple text list of book titles and their authors.
+"""
+def books_list(request):
+    books = Book.objects.all()
+    return HttpResponse(books)
