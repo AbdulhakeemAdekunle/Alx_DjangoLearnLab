6e4b486ab709e826dd80f679b75aa0f7ed5c1278
Abdulhakeem Adekunle <abdulhakeemyisa@gmail.com>
2024-08-27 09:29:28 +0100

    corrected a syntax error per checker requirements

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView, LogoutView, LoginView, register
+from .views import list_books, LibraryDetailView, LogoutView, LoginView
 
 urlpatterns = [
     path("", LibraryDetailView.as_view(), name='library'),
     path("book", list_books, name = "books-list"),
-    path('login/', LoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
-    path('register/', register, name='register'),
+    path('login/', LoginView.as_view(template_name="login.html"), name='login'),
+    path('logout/', LogoutView.as_view(template_name="logout.html"), name='logout'),
+    path('register/', views.register, name='register'),
 ]
