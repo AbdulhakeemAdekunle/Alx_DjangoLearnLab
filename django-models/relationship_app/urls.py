c60864fac078360588cbdd60da194c930eff5fb6
Abdulhakeem Adekunle <abdulhakeemyisa@gmail.com>
2024-09-21 17:52:50 +0100

    corrected a syntax error per checker requirement

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import LibraryDetailView, list_books
+from .views import list_books, LibraryDetailView
 
 urlpatterns = [
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library'),
