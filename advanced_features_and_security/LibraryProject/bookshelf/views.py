872e160835026962c6ecaeeed47240d2cc8a851a
abdulhakeem <abdulhakeemyisa@gmail.com>
2024-10-25 18:56:34 +0100

    update: I changed the views names

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -4,17 +4,17 @@ from django.contrib.auth.decorators import permission_required
 # Create your views here.
 
 permission_required('bookshelf.can_view_customer', raise_exception=True) # checks if a user has a view access
-def view_customer(request):
+def book_list(request):
     return HttpResponse('You can view book records') # displays message if the user belongs to the viewers' group
 
 permission_required('bookshelf.can_create_customer', raise_exception=True) # checks if a user has a create access
-def create_custome(request):
+def create_book(request):
     return HttpResponse('You can create book records') # displays message if the user belongs to the editors' group
 
 permission_required('bookshelf.can_edit_customer', raise_exception=True) # checks if a user has a edit access
-def edit_customer(request):
+def edit_edit(request):
     return HttpResponse('You can edit book records') # displays message if the user belongs to the editors' group
 
 permission_required('bookshelf.can_delete_customer', raise_exception=True) # checks if a user has a delete access
-def delete_customer(request):
+def delete_book(request):
     return HttpResponse('You can delete book records') # displays message if the user belongs to the admin group (only admin can delete a user record)
