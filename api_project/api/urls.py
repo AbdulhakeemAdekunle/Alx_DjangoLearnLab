03f53b92711eb5c49718c92b80dc9194612ae0ef
abdulhakeem <abdulhakeemyisa@gmail.com>
2024-10-27 18:36:33 +0100

    feat: implemented authentication and permission for the api endpoint

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken import views
 from .views import BookViewSet
 
 router = DefaultRouter()
@@ -7,4 +8,5 @@ router.register(r'books_all', BookViewSet, basename='book_all')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('auth', views.obtain_auth_token)
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from .models import Book
 from .serializers import BookSerializer
 
@@ -9,3 +10,4 @@ from .serializers import BookSerializer
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated, IsAdminUser]
