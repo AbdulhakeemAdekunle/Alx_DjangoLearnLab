f553334cef63a26bdca9f47ac378e4778945297a
abdulhakeem <abdulhakeemyisa@gmail.com>
2024-10-27 18:40:31 +0100

    refactor: added a default permission class in settings.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from .models import Book
 from .serializers import BookSerializer
 
@@ -10,4 +9,3 @@ from .serializers import BookSerializer
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [IsAuthenticated, IsAdminUser]
