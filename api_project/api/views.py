9c1cfd65caf27d3f98d8b3d4f637535248ae4c1c
abdulhakeem <abdulhakeemyisa@gmail.com>
2024-10-27 12:19:31 +0100

    refactor: rewrite serializers.py import line

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
-from rest_framework.generics import ListAPIView
+from rest_framework import generics
 from api.models import Book
 from api.serializers import BookSerializer
 
 # Create your views here.
 
 # book-list view that displays the list of books in db based of BookSerializer
-class BookList(ListAPIView):
+class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
